16eb47e27513352d429847928ca085dedfeaaaf9
Michał Górny <mgorny@gentoo.org>
2022-11-09 18:26:20 +0100

    Add a test for crate checksum verification
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- test/test_fetch.py
+++ test/test_fetch.py
@@ -1,3 +1,4 @@
+import hashlib
 import subprocess
 import unittest.mock
 
@@ -5,7 +6,7 @@ from pathlib import Path
 
 import pytest
 
-from pycargoebuild.fetch import fetch_crates
+from pycargoebuild.fetch import fetch_crates, verify_crates
 
 from .testdata import CRATES
 
@@ -24,4 +25,22 @@ def test_fetch_wget(mock_fetch):
     mock_fetch.assert_has_calls(
         [unittest.mock.call(["wget", "-O", str(test_path / crate.filename),
                              crate.crates_io_url])
-         for crate in CRATES])
+         for crate in CRATES], any_order=True)
+
+
+def hash_mocking_crate_gen(crates):
+    for crate in crates:
+        with unittest.mock.patch.object(hashlib, "sha256") as digest_mock:
+            digest_mock.return_value.hexdigest.return_value = crate.checksum
+            yield crate
+
+
+def test_verify():
+    test_path = Path("/test/path")
+    with unittest.mock.patch("pycargoebuild.fetch.open",
+                             unittest.mock.mock_open()) as open_mock:
+        open_mock.return_value.readinto.return_value = 0
+        verify_crates(hash_mocking_crate_gen(CRATES), test_path)
+    open_mock.assert_has_calls(
+        [unittest.mock.call(test_path / crate.filename, "rb", buffering=0)
+         for crate in CRATES], any_order=True)
