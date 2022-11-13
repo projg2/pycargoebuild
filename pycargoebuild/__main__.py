cec85b38c426c266d1df52e3b0bb16daf14b4ccf
Michał Górny <mgorny@gentoo.org>
2022-11-13 09:42:22 +0100

    Support pretty-printing logs using rich
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- pycargoebuild/__main__.py
+++ pycargoebuild/__main__.py
@@ -119,6 +119,20 @@ def main(prog_name: str, *argv: str) -> int:
 
 
 def entry_point() -> None:
+    try:
+        from rich.logging import RichHandler
+    except ImportError:
+        logging.basicConfig(
+            format="[{levelname:>7}] {message}",
+            level=logging.INFO,
+            style="{")
+    else:
+        logging.basicConfig(
+            format="{message}",
+            level=logging.INFO,
+            style="{",
+            handlers=[RichHandler(show_time=False, show_path=False)])
+
     sys.exit(main(*sys.argv))
 
 
