70e07391aa7ca7cfeffaee830929467d06b0fc43
Michał Górny <mgorny@gentoo.org>
2025-03-21 18:28:44 +0100

    Bump to 0.14.0
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- pycargoebuild/__init__.py
+++ pycargoebuild/__init__.py
@@ -1,7 +1,7 @@
 # pycargoebuild
-# (c) 2022-2024 Michał Górny <mgorny@gentoo.org>
+# (c) 2022-2025 Michał Górny <mgorny@gentoo.org>
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 """A generator for Rust/Cargo ebuilds written in Python"""
 
-__version__ = "0.13.5"
+__version__ = "0.14.0"
